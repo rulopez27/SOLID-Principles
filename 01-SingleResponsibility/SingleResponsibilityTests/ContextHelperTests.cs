049dde62a9a58da8424ec90ab046720fb1e2c2b0
rulopez <ruben_guille27@hotmail.com>
2021-05-27 00:39:59 -0700

    Added SingleResponsibilityTests project

---
+++ 01-SingleResponsibility/SingleResponsibilityTests/ContextHelperTests.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using Entities;
+using ContextHelper;
+using System.Linq;
+
+namespace SingleResponsibilityTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ListOfClients_IsNotEmpty()
+        {
+            List<Client> clients = new List<Client>();
+            clients = Helper.GetClientsList();
+            Assert.IsNotEmpty(clients);
+        }
+
+        [Test]
+        public void ClientFromList_HasListOfFavoriteMovies()
+        {
+            List<Client> clients = new List<Client>();
+            clients = Helper.GetClientsList();
+            Client firstClient = clients.FirstOrDefault();
+
+            Assert.IsNotEmpty(firstClient.FavoriteMovies);
+        }
+    }
+}
