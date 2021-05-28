92d742b0f09cd44f89ece9e1c615225f42b9c243
rulopez <ruben_guille27@hotmail.com>
2021-05-27 23:26:15 -0700

    BusinessLayer project added

---
+++ 01-SingleResponsibility/BusinessLayer/Recommender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+using System.Linq;
+
+namespace BusinessLayer
+{
+    public static class Recommender
+    {
+        public static List<Movie> MovieRecommendationsByClient(Client client, List<Movie> moviesCatalog)
+        {
+            var recommendations = moviesCatalog.Except(client.FavoriteMovies)
+                     .Where(movie => client.FavoriteMovies
+                     .Any(cm => cm.Genres.Intersect(movie.Genres).Any())).ToList();
+
+
+            return recommendations;
+        }
+    }
+}
